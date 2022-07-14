2ba11e0f6582ea8400a7e25d128725645011efde
Gustavo Oliveira <gustavooliveira120406@gmail.com>
2022-07-14 12:42:32 -0300

    Endpoint to get all registered posts defined.

--- src/api/entities/Post.js
+++ src/api/entities/Post.js
@@ -46,5 +46,19 @@ Post.insertPost = async post => { // Insert new post into database
     return result
 };
 
+Post.returnAllPosts = async () => { // Get all registered posts data
+    const result = getDatabaseQueryResult();
+
+    try {
+        const { rows: results, rowCount: total } = await conn.query('SELECT id, title FROM posts;');
+
+        result.setData({ results, total })
+    } catch {
+        result.setError(getDatabaseQueryError(500, 'Internal server error'))
+    };
+
+    return result
+};
+
 
 module.exports = Post
--- src/api/handlers/getAllPosts.js
+++ src/api/handlers/getAllPosts.js
@@ -1,4 +1,17 @@
-async function getAllPosts(request, response) {};
+const Post = require('../entities/Post');
+
+const { getErrorResponseBody, getSuccessResponseBody } = require('../../helpers');
+
+
+async function getAllPosts(request, response) {
+    const queryResult = await Post.returnAllPosts();
+
+    if (queryResult.error) {
+        return response.status(queryResult.error.code).json(getErrorResponseBody(queryResult.error.message))
+    };
+
+    return response.json(getSuccessResponseBody(queryResult.data))
+};
 
 
 module.exports = getAllPosts
