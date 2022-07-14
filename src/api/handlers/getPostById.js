d9e7aeaf33d5e9cb27f29666cd3d5e4457f369c0
Gustavo Oliveira <gustavooliveira120406@gmail.com>
2022-07-14 12:48:15 -0300

    "getPostById" middleware declared.

---
+++ src/api/handlers/getPostById.js
@@ -0,0 +1,4 @@
+async function getPostById(request, response) {};
+
+
+module.exports = getPostById
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 
 const createPost = require('../handlers/createPost');
 const getAllPosts = require('../handlers/getAllPosts');
+const getPostById = require('../handlers/getPostById');
 
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 
 router.post('/v1/posts', createPost);
 router.get('/v1/posts', getAllPosts);
+router.get('/v1/posts/:id', getPostById);
 
 
 module.exports = router
