e017f00aa7c57c52f4a7cb397b62e3317b34a782
Gustavo Oliveira <gustavooliveira120406@gmail.com>
2022-07-14 11:10:23 -0300

    Router middleware declared.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 
+const logger = require('./api/middlewares/logger');
+
+const router = require('./api/routes');
+
 
 const app = express();
+
+
+app.use(logger);
+
+app.use(express.json());
+
+app.use(router);
 
 
 module.exports = app
